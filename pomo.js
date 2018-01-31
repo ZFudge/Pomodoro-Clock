1b2f6df02165ecb2a65a5cff0521d8c21df1d3a3
Zach Fudge <zacheryfudge@gmail.com>
2018-01-30 22:07:29 -0700

    time setters

--- pomo.js
+++ pomo.js
@@ -4,11 +4,24 @@ const pomo = {
 	time: document.getElementById("time"),
 	status: document.getElementById("status"),
 	canvas: document.getElementById("pomodoro-canvas"),
+	break: {
+		time: 5,
+		text: document.getElementById('break-text')
+	},
+	session: {
+		time: 25,
+		text: document.getElementById('session-text')
+	},
+	start() {
+
+	},
 	main() {
 
 	},
-	draw() {
-
+	draw(t) {
+        this.context.beginPath();
+        this.context.arc(this.canvas.width/2,this.canvas.height/2,145,0,Math.PI * t);
+        this.context.stroke();
 	},
 	set timeText(t) {
 		this.time.innerHTML = t;
@@ -18,9 +31,21 @@ const pomo = {
 	},
 	set activate(ms) {
 		this.loop = setInterval(this.main,ms);
+	},
+	deactivate() {
+		clearInterval(this.loop);
+	},
+	set breakTime(t) {
+		this.break.time += t;
+		this.break.text = this.break.time;
+	},
+	set sessionTime(t) {
+		this.session.time += t;
+		this.session.text = this.session.time;
 	}
 
 };
 pomo.context = pomo.canvas.getContext("2d");
+pomo.context.lineWidth = 10;
 
 
