1941df75a61757875bc5714f28a5835f5bc73e1c
Zach Fudge <zacheryfudge@gmail.com>
2018-01-31 17:29:52 -0700

    timer working. starting/stopping on click, adjustable when inactive

--- pomo.js
+++ pomo.js
@@ -1,88 +1,104 @@
 const pomo = {
 	active: false,
 	ms: 1000,
-	time: {
-		paused: false,
-		seconds: 1500
-	},
 	session: {
 		text: document.getElementById('session-text'),
-		time: 25
+		time: 25,
+		set sessionTime(t) {
+			if ((this.time > 1 || t === 1) && !pomo.active) {
+				this.time += t;
+				const st = this.time;
+				this.text.innerHTML = st;
+				if (pomo.clock.status.innerHTML === "Session") {
+					pomo.clock.setSeconds = st;
+					pomo.clock.maxSecs = st;
+					pomo.clock.secondsToDigitalDisplay();
+				}
+			}
+		},
+		audio: new Audio("session.mp3")
 	},
 	break: {
 		text: document.getElementById('break-text'),
-		time: 5
+		time: 5,
+		set breakTime(t) {
+			if ((this.time > 1 || t === 1) && !pomo.active) {
+				this.time += t;
+				const st = this.time;
+				this.text.innerHTML = st;
+				if (pomo.clock.status.innerHTML === "Break") {
+					pomo.clock.setSeconds = st;
+					pomo.clock.maxSecs = st;
+					pomo.clock.secondsToDigitalDisplay();
+				}
+			}
+		},
+		audio: new Audio("break.mp3")
 	},
 	clock: {
 		status: document.getElementById("status"),
 		time: document.getElementById("time"),
-		canvas: document.getElementById("pomodoro-canvas")
+		canvas: document.getElementById("pomodoro-canvas"),
+		mouse: {
+			down: new Audio("mousedown.mp3"),
+			up: new Audio("mouseup.mp3")
+		},
+		paused: false,
+		seconds: 1500,
+		maxSecs: 1500,
+		set setSeconds(t) {
+			this.seconds = t * 60;
+		},
+		reduceSeconds() {
+			if (this.seconds > 0) {
+				this.seconds--;
+			} else {
+				this.flipStatus();
+			}
+		},
+		flipStatus() {
+			const newStatus = (this.status.innerHTML === "Session") ? "Break" : "Session";
+			this.status.innerHTML = newStatus;
+			this.setSeconds = pomo[newStatus.toLowerCase()].time;
+			this.maxSecs = this.seconds;
+			pomo[newStatus.toLowerCase()].audio.play();
+		},
+		secondsToDigitalDisplay() {
+			const t = this.seconds;
+			let hrs = 0, mins = 0, secs = 0;
+			if (t >= 3600) hrs = (t - t % 3600) / 3600;
+			secs = t % 60;
+			if (secs < 10) secs = `0${secs}`; 
+			if (hrs > 0) {
+				mins = ((t-3600) - (t % 60)) / 60; 
+				this.time.innerHTML = `${hrs}:${mins}:${secs}`;
+			} else {
+				mins = (t - (t % 60)) / 60;
+				this.time.innerHTML = `${mins}:${secs}`;
+			}
+		},
+		pause() {
+			this.paused = true;
+			clearInterval(this.loop);
+		},
+		loop: null
 	},
 	push() {
 		this.active = !this.active;
-		(this.active) ? this.start() : this.pause();
+		(this.active) ? this.start() : this.clock.pause();	
 	},
 	start() {
-		if (this.time.paused) {
-			this.loop = setInterval(this.main,ms);
-		} else {
-			this.time.seconds = session.time * 60;
-			this.loop = setInterval(this.main,ms);
-		}
-	},
-	pause() {
-		this.time.paused = true;
-		clearInterval(this.loop);
+		this.clock.loop = setInterval(this.count,this.ms);
 	},
 	draw(t) {
         this.clock.context.beginPath();
         this.clock.context.arc(this.clock.canvas.width/2,this.clock.canvas.height/2,145,0,Math.PI * t);
         this.clock.context.stroke();
 	},
-	set updateStatus(n) {
-
-	},
-	set timeText(t) {
-		this.clock.time.innerHTML = t;
-	},
-	set statusText(txt) {
-		this.clock.status.innerHTML = txt;
-	},
-	set breakTime(t) {
-		if (this.break.time > 1 || t === 1) {
-			this.break.time += t;
-			this.break.text.innerHTML = this.break.time;
-			if (this.clock.status.innerHTML === "Break") this.clock.statusText = this.break.time;
-		}
-	},
-	set seconds(t) {
-		this.time.seconds = t * 60;
-	},
-	set sessionTime(t) {
-		if (this.session.time > 1 || t === 1) {
-			this.session.time += t;
-			const st = this.session.time;
-			this.session.text.innerHTML = st;
-			this.seconds = st;
-			if (this.clock.status.innerHTML === "Session") this.secondsToDigitalDisplay();
-		}
-	},
-	secondsToDigitalDisplay() {
-		const t = this.time.seconds;
-		let hrs = 0, mins = 0, scs = 0;
-		if (t >= 3600) hrs = (t - t % 3600) / 3600;
-		scs = t % 60;
-		if (scs < 10) scs = `0${scs}`;
-		if (hrs > 0) {
-			mins = ((t-3600) - (t % 60)) / 60; 
-			this.clock.time.innerHTML = `${hrs}:${mins}:${scs}`;
-		} else {
-			mins = (t - (t % 60)) / 60;
-			this.clock.time.innerHTML = `${mins}:${scs}`;
-		}
-	},
-	main() {
-	},
+	count() {
+		pomo.clock.reduceSeconds();
+		pomo.clock.secondsToDigitalDisplay();
+	}
 
 };
 
